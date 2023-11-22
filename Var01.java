88ccb80f41bf60ba99f4a57046157dfd61617e38
Jalinsy <jiaji200324@gmail.com>
2023-11-22 23:19:46 +0900

    test

---
+++ Var01.java
@@ -0,0 +1,14 @@
+public class Var01 {
+	public static void main(String[] args) {
+		int age = 19;
+		double score = 99.6;
+		char gender = '男';
+		String name = "king";
+		System.out.println("人的信息如下:");
+		System.out.println("名字:" + name);
+		System.out.println("年龄:" + age);
+		System.out.println("体重:" + score);
+		System.out.println("性别:" + gender);
+		
+	}
+}
